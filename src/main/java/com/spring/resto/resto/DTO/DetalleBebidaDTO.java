f80f836bb5674e51a0cb8d689a94b3c5b8dc52cb
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-04-24 17:23:31 -0300

    IMPLEMENTACION DE PATRON DTO: PARA BEBIDADETALLE

--- src/main/java/com/spring/resto/resto/DTO/DetalleBebidaDTO.java
+++ src/main/java/com/spring/resto/resto/DTO/DetalleBebidaDTO.java
@@ -1,5 +1,58 @@
 package com.spring.resto.resto.DTO;
 
-public class DetalleBebidaDTO {
+import javax.validation.constraints.NotNull;
 
+import com.spring.resto.resto.entity.Bebida;
+import com.spring.resto.resto.entity.Ocupacion;
+
+public class DetalleBebidaDTO {
+	
+	@NotNull
+	private Long id;
+	
+	@NotNull
+	private Integer cantidad;
+	
+	private Bebida bebida;
+	
+	private Ocupacion ocupacion;
+	
+	public DetalleBebidaDTO(Long id,Integer cantidad, Bebida bebida,Ocupacion ocupacion) {
+		this.ocupacion = ocupacion;
+		this.id = id;
+		this.bebida = bebida;
+		this.cantidad = cantidad;
+	}
+	public DetalleBebidaDTO() {}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	public Bebida getBebida() {
+		return bebida;
+	}
+	public void setBebida(Bebida bebida) {
+		this.bebida = bebida;
+	}
+	public Ocupacion getOcupacion() {
+		return ocupacion;
+	}
+	public void setOcupacion(Ocupacion ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+	@Override
+	public String toString() {
+		return "DetalleBebidaDTO [id=" + id + ", cantidad=" + cantidad + ", bebida=" + bebida + ", ocupacion="
+				+ ocupacion + "]";
+	}
+	
+	
 }
--- src/main/java/com/spring/resto/resto/DTO/DetalleBebidaRequestDTO.java
+++ src/main/java/com/spring/resto/resto/DTO/DetalleBebidaRequestDTO.java
@@ -1,5 +1,54 @@
 package com.spring.resto.resto.DTO;
 
-public class DetalleBebidaRequestDTO {
+import javax.validation.constraints.NotNull;
 
+import com.spring.resto.resto.entity.Bebida;
+import com.spring.resto.resto.entity.Ocupacion;
+
+public class DetalleBebidaRequestDTO {
+	
+	@NotNull
+	private Integer cantidad;
+	private Bebida bebida;
+	private Ocupacion ocupacion;
+	
+	public DetalleBebidaRequestDTO(Integer cantidad,Bebida bebida , Ocupacion ocupacion) {
+		this.cantidad = cantidad;
+		this.bebida = bebida;
+		this.ocupacion = ocupacion;
+	}
+	
+	public DetalleBebidaRequestDTO() {}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Bebida getBebida() {
+		return bebida;
+	}
+
+	public void setBebida(Bebida bebida) {
+		this.bebida = bebida;
+	}
+
+	public Ocupacion getOcupacion() {
+		return ocupacion;
+	}
+
+	public void setOcupacion(Ocupacion ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+
+	@Override
+	public String toString() {
+		return "DetalleBebidaRequestDTO [cantidad=" + cantidad + ", bebida=" + bebida + ", ocupacion=" + ocupacion
+				+ "]";
+	}
+	
+	
 }
