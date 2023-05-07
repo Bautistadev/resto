e5058decf91e9d850a86416337f4d050763652bf
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-05-06 21:10:42 -0300

    IMPLEMENTACION DE LA SEGURIDAD DE SPRING

--- src/main/java/com/spring/resto/resto/security/service/utils/JwtTokenUtils.java
+++ src/main/java/com/spring/resto/resto/security/service/utils/JwtTokenUtils.java
@@ -3,5 +3,16 @@ package com.spring.resto.resto.security.service.utils;
 import java.io.Serializable;
 
 public class JwtTokenUtils implements Serializable{
+
+	private static final long serialVersionUID = -2550185165626007488L;
+	
+	//TIEMPO DE VALIDACION DEL TOKEN
+	public static final long JWT_TOKEN_VALIDITY = 30*60*60*100;
+	
+	//TIMEPO DE ACTUALIZACION DEL TOKEN
+	public static final long JWT_REFRESH_TOKEN_VALIDITY = 4*60*60*1000;
+	
+	private String secret;
+	
 	
 }
