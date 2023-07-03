2fa0fb21a987528aae2783af5431aa6552db2639
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-07-03 01:34:58 -0300

    Implementacion de las categorias

---
+++ src/main/java/com/spring/resto/resto/entity/Categoria.java
@@ -0,0 +1,59 @@
+package com.spring.resto.resto.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.spring.resto.resto.enums.CategoriaEnum;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private CategoriaEnum categoria;
+
+	public Categoria(Long id, @NotNull CategoriaEnum categoria) {
+		super();
+		this.id = id;
+		this.categoria = categoria;
+	}
+
+	public Categoria() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public CategoriaEnum getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaEnum categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", categoria=" + categoria + "]";
+	}
+	
+	
+	
+}
